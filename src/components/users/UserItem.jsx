d6fe9688b8f8511b0ac482a9108cb39fabc57271
Brad Traversy <traversymedia@gmail.com>
2021-11-05 10:09:18 -0400

    Display users

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function UserItem({ user: { login, avatar_url } }) {
+  return (
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className='flex-row items-center space-x-4 card-body'>
+        <div>
+          <div className='avatar'>
+            <div className='rounded-full shadow w-14 h-14'>
+              <img src={avatar_url} alt='Profile' />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className='card-title'>{login}</h2>
+          <Link
+            className='text-base-content text-opacity-40'
+            to={`/users/${login}`}
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+}
+
+export default UserItem
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Spinner from '../layout/Spinner'
+import UserItem from '../users/UserItem'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -26,7 +27,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
