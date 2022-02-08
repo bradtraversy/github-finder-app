4d93c8d744c238feee30548a4d565959b5b6d9e6
rob <robert_regan86@yahoo.com>
2022-02-08 16:46:04 -0500

    fixed the blog link

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -99,7 +99,7 @@ function User() {
                   <div className='stat-title text-md'>Website</div>
                   <div className='text-lg stat-value'>
                     <a
-                      href={`https://${blog}`}
+                      href={`${blog}`}
                       target='_blank'
                       rel='noreferrer'
                     >
