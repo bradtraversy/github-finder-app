b60283dae82fca616fd14d0ca1146c5155a8586e
Will Adams <bushblade@gmail.com>
2022-02-09 11:42:41 +0000

    visibility so no content shift

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -4,9 +4,12 @@ import AlertContext from '../../context/alert/AlertContext'
 function Alert() {
   const { alert } = useContext(AlertContext)
 
-  return alert ? (
-    <div className='flex items-start mb-4'>
-      <div className='alert alert-error pr-8'>
+  return (
+    <div
+      className={`grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-8 mb-8`}
+      style={{ visibility: alert ? 'visible' : 'hidden' }}
+    >
+      <div className='alert alert-error flex justify-start'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
@@ -20,10 +23,10 @@ function Alert() {
             d='M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636'
           ></path>
         </svg>
-        <label>{alert.msg}</label>
+        <p>{alert?.msg}</p>
       </div>
     </div>
-  ) : null
+  )
 }
 
 export default Alert
