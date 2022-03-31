3817af4136574eb9cd7b0dcd058e478f813e1bfd
Will Adams <bushblade@gmail.com>
2022-03-31 11:56:52 +0100

    remove craco and update to React 18

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
