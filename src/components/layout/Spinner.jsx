7be17b6b0c40fc4ebdc49d901ab48a273e02bc34
Will Adams <bushblade@gmail.com>
2022-10-14 09:08:31 +0100

    remove na className

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -2,7 +2,7 @@ import spinner from './assets/spinner.gif'
 
 function Spinner() {
   return (
-    <div className='w-100 mt-20'>
+    <div className='mt-20'>
       <img
         width={180}
         className='text-center mx-auto'
