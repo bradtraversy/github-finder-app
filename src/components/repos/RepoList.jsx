3ab3d5a0443666d99a996fba641f65b9c272c555
Hugo Botas <263580+hugobotas@users.noreply.github.com>
2023-01-20 12:17:35 +0000

    Update RepoList.jsx
    Typo

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -6,7 +6,7 @@ function RepoList({ repos }) {
     <div className='rounded-lg shadow-lg card bg-base-100'>
       <div className='card-body'>
         <h2 className='text-3xl my-4 font-bold card-title'>
-          Latest Respositories
+          Latest Repositories
         </h2>
         {repos.map((repo) => (
           <RepoItem key={repo.id} repo={repo} />
